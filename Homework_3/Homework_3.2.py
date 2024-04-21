c7343e3b812e0522d6643ee70c1deb320a10f1c5
alina-kylymnyk <kylymnykalina111@gmail.com>
2024-04-21 21:27:19 +0300

    add homework 3

---
+++ Homework_3/Homework_3.2.py
@@ -0,0 +1,14 @@
+import random
+
+
+def get_numbers_ticket(min_num, max_num, quantity):
+    if min_num >= max_num or quantity > (max_num - min_num + 1):
+        print("Неправильно вказаний формат чисел.")
+        return None
+
+    numbers = random.sample(range(min_num, max_num), quantity)
+    return (numbers)
+
+
+lottery_numbers = get_numbers_ticket(1, 49, 6)
+print("Ваші лотерейні числа:", lottery_numbers)
