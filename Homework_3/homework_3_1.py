949d4643a461ce520527b9b6edcceb4d0dc0c9df
alina-kylymnyk <kylymnykalina111@gmail.com>
2024-04-22 21:37:14 +0300

    add homework 3

--- Homework_3/homework_3_1.py
+++ Homework_3/homework_3_1.py
@@ -1,16 +1,14 @@
 from datetime import datetime
 
+
 def get_days_from_today(date):
     try:
         date_obj = datetime.strptime(date, "%Y-%m-%d").date()
         current_date = datetime.now().date()
         delta = current_date - date_obj
-        print(delta.days)
+        return delta.days
     except ValueError:
         print("Неправильно введена дата. Введіть дату у форматі YYYY-MM-DD.")
 
 
 get_days_from_today("2023-10-09")
-
-
-
--- Homework_3/homework_3_2.py
+++ Homework_3/homework_3_2.py
@@ -1,14 +1,12 @@
 import random
 
-
 def get_numbers_ticket(min_num, max_num, quantity):
     if min_num >= max_num or quantity > (max_num - min_num + 1):
         print("Неправильно вказаний формат чисел.")
-        return None
+        return []
 
-    numbers = random.sample(range(min_num, max_num), quantity)
-    return (numbers)
-
+    numbers = random.sample(range(min_num, max_num + 1), quantity)
+    return sorted(numbers)
 
 lottery_numbers = get_numbers_ticket(1, 49, 6)
 print("Ваші лотерейні числа:", lottery_numbers)
